aa5c9e005496f7ce57929b283cc5d6575d6377d7
Juan Jose Rendon <juanjoserendon13@outlook.com>
2019-09-18 12:04:43 -0500

    basic items displayed  and methods added

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button class="del" @click="$emit('del-todo', todo.id)">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props:["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 1rem;
+    border-bottom: 0.1rem #ccc dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 0.5rem 0.9rem;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
