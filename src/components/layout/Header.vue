833a5c2ca73b0366c39dfceb20c5686c5320ccdb
Juan Jose Rendon <juanjoserendon13@outlook.com>
2019-09-18 14:20:29 -0500

    header component added

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 1rem;
+}
+.header a {
+  color: #fff;
+  padding-right: 0.5rem;
+}
+</style>
